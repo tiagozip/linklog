809a2720d5659013f483c775ed82cebd2826ab63
Tiago <70700766+tiagorangel1@users.noreply.github.com>
2025-06-10 11:22:40 +0100

    dashboard: add active link indicator

--- public/dash/index.js
+++ public/dash/index.js
@@ -982,8 +982,17 @@ const views = {
   async showLinkDetails(slug, preloadedResponse = null) {
     const key = localStorage.getItem(`linklog--${slug}`);
 
-    $("main").innerHTML =
-      `<div class="loader-wrap">
+    document.querySelectorAll("aside .link").forEach((el) => {
+      if (el.classList.contains("active")) {
+        el.classList.remove("active");
+      }
+
+      if (el.innerText.replace("/", "") === slug) {
+        el.classList.add("active");
+      }
+    });
+
+    $("main").innerHTML = `<div class="loader-wrap">
         <svg width="32" height="32" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><style>.spinner_ajPY{transform-origin:center;animation:spinner_AtaB .4s infinite linear;fill:white}@keyframes spinner_AtaB{100%{transform:rotate(360deg)}}</style><path d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z" opacity=".25" style="fill:white"/><path d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z" class="spinner_ajPY"/></svg>
       </div>`;
 
@@ -1232,8 +1241,7 @@ const views = {
     `;
 
     $(".confirm-delete").addEventListener("click", async () => {
-      $("main").innerHTML =
-        `<div class="loader-wrap">
+      $("main").innerHTML = `<div class="loader-wrap">
         <svg width="32" height="32" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><style>.spinner_ajPY{transform-origin:center;animation:spinner_AtaB .4s infinite linear;fill:white}@keyframes spinner_AtaB{100%{transform:rotate(360deg)}}</style><path d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z" opacity=".25" style="fill:white"/><path d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z" class="spinner_ajPY"/></svg>
       </div>`;
 
@@ -1272,8 +1280,7 @@ const views = {
   },
 
   async createNewLink(url, captchaToken) {
-    $("main").innerHTML =
-      `<div class="loader-wrap">
+    $("main").innerHTML = `<div class="loader-wrap">
         <svg width="32" height="32" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><style>.spinner_ajPY{transform-origin:center;animation:spinner_AtaB .4s infinite linear;fill:white}@keyframes spinner_AtaB{100%{transform:rotate(360deg)}}</style><path d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z" opacity=".25" style="fill:white"/><path d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z" class="spinner_ajPY"/></svg>
       </div>`;
 
